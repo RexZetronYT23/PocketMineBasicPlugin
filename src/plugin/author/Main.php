75f6985576ef9f43d2ff25aad94cb4831fd38159
RexZetronYT23 <57451491+RexZetronYT23@users.noreply.github.com>
2021-02-10 10:15:20 +0100

    Create Main.php

---
+++ src/plugin/author/Main.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace plugin\author;
+
+use pocketmine\plugin\PluginBase; #Für die Main datei
+use pocketmine\event\Listener; #Für Events
+
+class Main extends PluginBase implements Listener {
+
+}
