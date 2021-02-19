68940ba6527278a0faee88186cd28d1cedcd5640
RexZetronYT23 <57451491+RexZetronYT23@users.noreply.github.com>
2021-02-19 20:31:38 +0100

    Init JoinListener
    Added JoinListener and register that multifile event JoinListener.php

--- src/plugin/author/Main.php
+++ src/plugin/author/Main.php
@@ -4,7 +4,12 @@ namespace plugin\author;
 
 use pocketmine\plugin\PluginBase; #Für die Main datei
 use pocketmine\event\Listener; #Für Events
+use plugin\author\events\JoinListener;
 
 class Main extends PluginBase implements Listener {
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents(new JoinListener($this), $this);
+    }
 
 }
